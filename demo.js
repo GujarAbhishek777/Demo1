2a20ff833b45c94bc059be72660481fe79e10539
AbhishekGujar <gujarabhishek777@gmail.com>
2022-11-06 00:06:46 +0530

    add new demo.js file and added html content to see getElementById

---
+++ demo.js
@@ -0,0 +1,36 @@
+//console.dir(document);
+//console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title=123;
+// console.log(document.doctype);
+// console.log(document.body);
+// console.log(document.head);
+// console.log(document.all);
+// console.log(document.all[10]);
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
+
+
+//getElementById
+// console.log(document.getElementById('header-title'));
+var headerTitle=document.getElementById('header-title');
+// console.log(headerTitle);
+// var header=document.getElementById('main-header');
+// var main1=document.getElementById('main');
+
+// headerTitle.textContent='Abhishek';
+// headerTitle.innerText='Abhi123';
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML='<h3>Hello Abhishek</h3>';
+// headerTitle.style.borderBottom='solid 4px #000';
+// header.style.borderBottom='solid 4px #000';
+// document.getElementById('main').style.fontSize="100px";
+// document.getElementById("header-title").style.fontSize = "x-large";
+document.getElementById("abhi").style.fontSize = "50px";
+document.getElementById("abhi").style.color = "green";
+document.getElementById("abhi").innerHTML = '<b>Items</b>';
+
+
+
